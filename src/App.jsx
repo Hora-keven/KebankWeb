e1cf32896bb2bde6b22aefdfc9ca884470b2f194
hora-keven <kevensantos@gmail.com>
2023-10-17 11:31:19 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import './components/Header.css'
 import Header from './components/Header'
 import Content from './components/Content'
-import './components/Content.css'
+import './components/global.css'
 import Footer from './components/Footer'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
